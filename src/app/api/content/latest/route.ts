27ed32ba82baa5010393952e0a6146255fee1803
SingIt Pop Deploy <deploy@singitpop.com>
2026-01-22 13:08:58 +0000

    fix: change cover image extension from .jpg to .png

--- src/app/api/content/latest/route.ts
+++ src/app/api/content/latest/route.ts
@@ -76,7 +76,7 @@ export async function GET() {
                     // Only singles have cover images in S3
                     const albumSlug = album.id;
                     const trackTitle = encodeURIComponent(matchingTrack.title);
-                    backgroundCoverArt = `https://singitpop-music.s3.eu-north-1.amazonaws.com/albums/${albumSlug}/${trackTitle}/cover.jpg`;
+                    backgroundCoverArt = `https://singitpop-music.s3.eu-north-1.amazonaws.com/albums/${albumSlug}/${trackTitle}/cover.png`;
                     break;
                 }
             }
@@ -86,7 +86,7 @@ export async function GET() {
         if (latestSingle) {
             const albumSlug = latestSingle.albumId;
             const trackTitle = encodeURIComponent(latestSingle.title);
-            latestSingleTrackCover = `https://singitpop-music.s3.eu-north-1.amazonaws.com/albums/${albumSlug}/${trackTitle}/cover.jpg`;
+            latestSingleTrackCover = `https://singitpop-music.s3.eu-north-1.amazonaws.com/albums/${albumSlug}/${trackTitle}/cover.png`;
         }
 
         return NextResponse.json({
