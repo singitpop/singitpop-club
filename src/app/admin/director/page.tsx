118631d477a6270cef2b3946e368545b4b22fc16
SingIt Pop Deploy <deploy@singitpop.com>
2026-02-12 00:41:08 +0000

    feat: wire up DirectorWizard to /admin/director route

---
+++ src/app/admin/director/page.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+
+import { DirectorWizard } from '@/components/director/DirectorWizard';
+
+export default function DirectorPage() {
+    return (
+        <main className="min-h-screen bg-black">
+            <DirectorWizard />
+        </main>
+    );
+}
